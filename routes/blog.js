acfe859989b86ae40a3d601ae74ad67ff214700a
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2016-09-30 18:34:56 -0400

    made blog page

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET blog listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('blog', { title: 'blog' });
 });
 
 module.exports = router;
