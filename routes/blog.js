7b3ba46d6171be9a6b329b160f571020102fd027
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2016-10-01 17:56:01 -0400

    much MVC

--- routes/blog.js
+++ routes/blog.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET blog listing. */
 router.get('/', function(req, res, next) {
-  res.render('blog', { title: 'blog' });
+  res.render('blog', { title: 'blog',
+                         css: 'blog'});
 });
 
 module.exports = router;
